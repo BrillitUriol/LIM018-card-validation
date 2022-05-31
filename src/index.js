899cb2cf68a19a5308d925064a00f92f338aedfb
Brillit <brillituriol23@gmail.com>
2022-05-30 22:32:07 -0500

    agregando cambios css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import validator from './validator.js';
 
+ //var elemento = document.querySelector("#micampoid");
+ //var elementoclass = document.querySelector(".micampoid");
 
 document.addEventListener('DOMContentLoaded', () => {
 
     var cardNumberField = document.getElementById("cardNumber"),
-    btnForm = document.getElementById("confirm-purchase");
+    btnForm = document.getElementById("confirm-purchase"),
+    btnBuy = document.querySelector("#buy"),
+    screen1 = document.querySelector("#screen1"),
+    screen2 = document.querySelector("#screen2");
     
     btnForm.addEventListener("click", (e) => {
         e.preventDefault();
@@ -14,6 +19,13 @@ document.addEventListener('DOMContentLoaded', () => {
         {
             cardNumberField.classList.add("credit-card-error");
         }
+    });
+    btnBuy.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        screen2.classList.remove("ocultar");
+        screen1.classList.add("ocultar");
+
     });
     cardNumberField.addEventListener("keyup", (e) => {
         e.preventDefault();          
